ac7705c3b9f0e8f40717a3b63c327b559bcdc5fc
Guillaume Moutier <guillaume.moutier@gmail.com>
2022-09-06 17:23:20 -0400

    wip - pre-send to prediction

--- classification/container/app.py
+++ classification/container/app.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 from pydantic import BaseModel
 from typing import Tuple
+from glob import glob
 
 # App creation
 app = FastAPI()
@@ -24,11 +25,18 @@ app.add_middleware(
 class TileEntry(BaseModel):
     coordinates: Tuple[float,float] = None
     kind: str = ""
-    ill: bool = False
+    status: str = ""
+    disease: str = ""
+    frame: int = None
 
 # Output data class
 class TileStatus(BaseModel):
     status: str = ""
+
+# Initialize picture banks arrays with full relative path
+wheat_healthy = sorted(glob(os.path.join('./assets/pictures/wheat_healthy/','*')))
+wheat_brown_rust = sorted(glob(os.path.join('./assets/pictures/wheat_brown_rust/','*')))
+wheat_yellow_rust = sorted(glob(os.path.join('./assets/pictures/wheat_yellow_rust/','*')))
 
 def calculatePath(start_coordinates, goal_coordinates):
     path, length = environment.find_shortest_path(start_coordinates, goal_coordinates)
@@ -45,10 +53,31 @@ async def root():
 @app.post("/classify", response_model = TileStatus)
 async def classify(entry: TileEntry):
     response = TileStatus()
-    if entry.ill:
-        response.status = "ill"
-    else:
-        response.status = "healthy"
+    
+    #TODO Remove debug
+    picture_path = ""
+    print(entry.disease)
+    print(entry.frame)
+
+    if (entry.kind == "wheat"):
+        if (entry.disease == "wheat_healthy"):
+            picture_path = wheat_healthy[entry.frame]
+        if (entry.disease == "wheat_brown_rust"):
+            picture_path = wheat_brown_rust[entry.frame]
+        if (entry.disease == "wheat_yellow_rust"):
+            picture_path = wheat_yellow_rust[entry.frame]
+
+    #TODO Remove debug
+    print(picture_path)
+
+    #TODO Send picture to prediction service
+            
+    #if entry.ill:
+    #    response.status = "ill"
+    #else:
+    #    response.status = "healthy"
+    
+    response.status = "healthy"
     return response
     
 # Launch the FastAPI server
