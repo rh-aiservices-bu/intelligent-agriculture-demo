3b7297de9fec5471dada29733bbf5065391f22b6
Guillaume Moutier <guimou@users.noreply.github.com>
2023-05-03 15:27:05 -0400

    update (#5)
    * now uses an inference endpoint
    
    * wip-all at once
    
    * working with REST call
    
    * cleanup
    
    * publication
    
    * start drone and splash screen
    
    * serve frontend from main app
    
    * adjust timeout
    
    * cleanup ui and training
    
    * cleanup tractor routing notebook
    
    * cleanup pathfinder notebook
    
    * path container ok
    
    * dockerfile classification
    
    * deployment ok

--- classification/container/app.py
+++ classification/container/app.py
@@ -1,19 +1,35 @@
 """ Classification API. Receives field data and sends back prediction. """
+import io
 import os
+import uuid
 from glob import glob
 from typing import Tuple
 
+import numpy as np
 import requests
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from numpy import argmax, array
+from numpy import max as max_
+from PIL import Image
 from pydantic import BaseModel
+from tensorflow import expand_dims
+from tensorflow.keras.utils import img_to_array, load_img
 from uvicorn import run
 
 # Load local env vars if present
 load_dotenv()
-PREDICTION_ENDPOINT = os.getenv('PREDICTION_ENDPOINT')
-PATH_ENDPOINT = os.getenv('PATH_ENDPOINT')
+# External services
+INFERENCE_ENDPOINT = os.getenv('INFERENCE_ENDPOINT', '')
+PATHSERVICE_ENDPOINT = os.getenv('PATHSERVICE_ENDPOINT', '')
+# Frontend configuration - (PATHSERVICE_ENDPOINT already initialized)
+CLASSIFICATION_ENDPOINT = os.environ.get('CLASSIFICATION_ENDPOINT', 'http://localhost:5002')
+DRONE_SPEED = float(os.environ.get('DRONE_SPEED', '0.5'))
+TRACTOR_SPEED = float(os.environ.get('TRACTOR_SPEED', '0.1'))
+COMM_SPEED = float(os.environ.get('COMM_SPEED', '1'))
+WEALTHY_CROP_INITIAL_PERCENTAGE = int(os.environ.get('WEALTHY_CROP_INITIAL_PERCENTAGE', '50'))
 
 # App creation
 app = FastAPI()
@@ -30,7 +46,26 @@ app.add_middleware(
     allow_headers=headers
 )
 
-# Input data classes
+# Inference classes
+class_predictions = array([
+    'Corn___Common_Rust',
+    'Corn___Gray_Leaf_Spot',
+    'Corn___Healthy',
+    'Corn___Northern_Leaf_Blight',
+    'Potato___Early_Blight',
+    'Potato___Healthy',
+    'Potato___Late_Blight',
+    'Rice___Brown_Spot',
+    'Rice___Healthy',
+    'Rice___Leaf_Blast',
+    'Rice___Neck_Blast',
+    'Wheat___Brown_Rust',
+    'Wheat___Healthy',
+    'Wheat___Yellow_Rust'
+])
+
+## FastAPI classes
+# Input
 class TileEntry(BaseModel):
     """ Data about a field received from the drone """
     coordinates: Tuple[float,float] = None
@@ -40,12 +75,35 @@ class TileEntry(BaseModel):
     frame: int = None
     uuid: str = ""
 
-# Output data classes
+    class Config:
+        """ Example """
+        schema_extra = {
+            "example": {
+                "coordinates": (715,822),
+                "kind": "corn",
+                "status": "ill",
+                "disease": "Corn___Common_Rust",
+                "frame": 3,
+                "uuid": "c303282d-f2e6-46ca-a04a-35d3d873712d"
+            }
+        }
+
+# Output
 class TileStatus(BaseModel):
     """ Data sent back to the drone after model prediction """
     status: str = ""
     model_prediction: str = ""
     model_prediction_confidence_score: float = None
+
+    class Config:
+        """ Example """
+        schema_extra = {
+            "example": {
+                "status": "ill",
+                "model_prediction": "Corn___Common_Rust",
+                "model_prediction_confidence_score": 0.95
+            }
+        }
 
 # Initialize picture banks arrays with full relative path
 wheat_healthy = sorted(glob(os.path.join('./assets/pictures/wheat_healthy/','*')))
@@ -62,12 +120,12 @@ potato_late_blight = sorted(glob(os.path.join('./assets/pictures/potato_late_bli
 def add_path_entry(kind,coordinates,uuid):
     """ Calls the API to add a field to the array of places to visit """
     data_json = {"kind": kind, "coordinates": coordinates, "uuid": uuid}
-    resp = requests.put(url = PATH_ENDPOINT + 'destination', json=data_json, timeout=5)
-    print(resp.json())
+    resp = requests.put(url = PATHSERVICE_ENDPOINT + '/destination', json=data_json, timeout=10)
+    print('Path entry added')
 
-# Base API
-@app.get("/")
-async def root():
+# Status API
+@app.get("/status")
+async def status():
     """ Simple status check """
     return {"message": "Status:OK"}
 
@@ -102,11 +160,38 @@ async def classify(entry: TileEntry):
         if entry.disease == "Potato___Late_Blight":
             picture_path = potato_late_blight[entry.frame]
 
-    file =  {'file': open(picture_path, 'rb')}
-    resp = requests.post(url = PREDICTION_ENDPOINT, files = file, timeout=5)
+    #file =  {'file': open(picture_path, 'rb')}
 
-    result = resp.json()
+    img = load_img(picture_path, target_size=(200, 200))
+    img_array = img_to_array(img) # Transform image to array
+    img_array = expand_dims(img_array, 0) # Expand dimension as expected by inference point
 
+    # json payload
+    img_numpy = img_array.numpy() # Convert to numpy array
+    im_json = img_numpy.tolist() # Converts to a nested list for json payload
+    
+    # ModelMesh expected input format
+    # (get model input "name" and "shape" from your model)
+    data = {
+        "inputs": [
+            { 
+                "name": "input_1",
+                "shape": [1,200,200,3],
+                "datatype": "FP32",
+                "data": im_json
+            }
+        ]
+    }
+
+    # Call the inference point
+    response = requests.post(INFERENCE_ENDPOINT, json=data)
+    raw_output = response.json() # Extract to Json
+    arr = np.array(raw_output['outputs'][0]['data']) # Get the response data as a NumPy Array
+    # Retrieve result
+    class_prediction = class_predictions[argmax(arr)]
+    score = max_(arr)
+    model_score = round(score * 100, 2)
+    result = {}
     if entry.disease in ["Wheat___Healthy","Corn___Healthy","Potato___Healthy"]:
         result['status'] = 'healthy'
     else:
@@ -115,10 +200,29 @@ async def classify(entry: TileEntry):
 
     response = TileStatus()
     response.status = result['status']
-    response.model_prediction = result['model_prediction']
-    response.model_prediction_confidence_score = result['model_prediction_confidence_score']
+    response.model_prediction = class_prediction
+    response.model_prediction_confidence_score = model_score
 
     return response
+
+# Frontend configuration
+@app.get("/config.json")
+async def status():
+    """ Returns the configuration needed by the frontend """
+    # Build the JSON object based on environment variables
+    config = {
+    'classificationEndpoint': CLASSIFICATION_ENDPOINT,
+    'pathserviceEndpoint': PATHSERVICE_ENDPOINT,
+    'droneSpeed': DRONE_SPEED,
+    'tractorSpeed': TRACTOR_SPEED,
+    'commSpeed': COMM_SPEED,
+    'wealthyCropInitialPercentage': WEALTHY_CROP_INITIAL_PERCENTAGE
+    }
+
+    return config
+
+# Frontend serving
+app.mount("/", StaticFiles(directory="public", html=True), name="public")
 
 # Launch the FastAPI server
 if __name__ == "__main__":
--- model/serving/container/startdev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watchmedo auto-restart --pattern "*.py" --recursive --signal SIGTERM python app.py
